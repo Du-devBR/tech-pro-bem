c5109bd5be4d29b4107d66c92ca286d452c33e97
Du-devBR <eduardo.ananiasdev@outlook.com>
2024-06-05 19:45:58 -0300

    feat: Alterar logo do header e adicionar elemento avatar

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,21 +1,19 @@
 import Image from "next/image";
 import logo from "../../../public/logo.png";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 export function Header() {
   return (
-    <header>
-      <div className="flex items-center gap-4 py-8">
+    <header className="flex items-center justify-between py-4 border-b-[1px] border-zinc-300">
+      <div className="flex items-center gap-4 ">
         <Image
-          className="w-20"
+          className="max-w-72"
           src={logo}
           alt="Logotipo E-nn, background preto e desenho de oculos na cor branca."
         />
-        <div className="flex flex-col">
-          <span className="text-3xl font-bold">E-NN</span>
-          <span className="text-xl font-light text-zinc-700">
-            Suas noticias estão aqui!
-          </span>
-        </div>
       </div>
+      <Avatar>
+        <AvatarImage src="https://avatars.githubusercontent.com/u/9919?v=4" />
+      </Avatar>
     </header>
   );
 }
